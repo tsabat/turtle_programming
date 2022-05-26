9cbd35da459518556c89f0694adedbad6dcdf8ff
Parker Sabat <parkersabat@MacBook-Pro-3.localdomain>
2022-05-25 19:20:07 -0700

    before first push

--- turtle_tag_2p.py
+++ turtle_tag_2p.py
@@ -12,95 +12,20 @@ i = True
 tt = getscreen()
 
 
-def make_rocks(level):
-    if level == 1:
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 200)
-        rock_coordinates1 = (rock_pos1, rock_pos2)
+def make_rock_new(count):
+    coordintates = []
+    counter = 0
+    grid_size = 400
+    while counter < count:
+        p1 = randrange(-grid_size, grid_size)
+        p2 = randrange(-grid_size, grid_size)
         rock = Turtle("square")
         rock.color("gray")
-        rock.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 100)
-        rock_pos2 = randrange(-100, 200)
-        rock_coordinates2 = (rock_pos1, rock_pos2)
-        rock2 = Turtle("square")
-        rock2.color("gray")
-        rock2.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 300)
-        rock_coordinates3 = (rock_pos1, rock_pos2)
-        rock3 = Turtle("square")
-        rock3.color("gray")
-        rock3.setpos(rock_pos1, rock_pos2)
-    elif level == 2:
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 200)
-        rock_coordinates4 = (rock_pos1, rock_pos2)
-        rock = Turtle("square")
-        rock.color("gray")
-        rock.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 100)
-        rock_pos2 = randrange(-100, 200)
-        rock_coordinates5 = (rock_pos1, rock_pos2)
-        rock2 = Turtle("square")
-        rock2.color("gray")
-        rock2.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 300)
-        rock_coordinates6 = (rock_pos1, rock_pos2)
-        rock3 = Turtle("square")
-        rock3.color("gray")
-        rock3.setpos(rock_pos1, rock_pos2)
-    elif level == 3:
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 200)
-        rock_coordinates7 = (rock_pos1, rock_pos2)
-        rock = Turtle("square")
-        rock.color("gray")
-        rock.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 100)
-        rock_pos2 = randrange(-100, 200)
-        rock_coordinates8 = (rock_pos1, rock_pos2)
-        rock2 = Turtle("square")
-        rock2.color("gray")
-        rock2.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 300)
-        rock_coordinates9 = (rock_pos1, rock_pos2)
-        rock3 = Turtle("square")
-        rock3.color("gray")
-        rock3.setpos(rock_pos1, rock_pos2)
-    elif level == 4:
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 200)
-        rock_coordinates10 = (rock_pos1, rock_pos2)
-        rock = Turtle("square")
-        rock.color("gray")
-        rock.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 100)
-        rock_pos2 = randrange(-100, 200)
-        rock_coordinates11 = (rock_pos1, rock_pos2)
-        rock2 = Turtle("square")
-        rock2.color("gray")
-        rock2.setpos(rock_pos1, rock_pos2)
-        rock_pos1 = randrange(-200, 200)
-        rock_pos2 = randrange(-200, 300)
-        rock_coordinates12 = (rock_pos1, rock_pos2)
-        rock3 = Turtle("square")
-        rock3.color("gray")
-        rock3.setpos(rock_pos1, rock_pos2)
-    print(rock_coordinates1)
-    print(rock_coordinates2)
-    print(rock_coordinates3)
-    print(rock_coordinates4)
-    print(rock_coordinates5)
-    print(rock_coordinates6)
-    print(rock_coordinates7)
-    print(rock_coordinates8)
-    print(rock_coordinates9)
-    print(rock_coordinates10)
-    print(rock_coordinates11)
-    print(rock_coordinates12)
+        rock.setpos(p1, p2)
+        coordintates.append((p1, p2))
+        counter += 1
+
+    return coordintates
 
 
 tager = Turtle("turtle")
@@ -111,10 +36,12 @@ player = Turtle("turtle")
 player.pencolor("blue")
 player.pensize(5)
 
-make_rocks(1)
-make_rocks(2)
-make_rocks(3)
-make_rocks(4)
+coordinates = make_rock_new(16)
+print(coordinates)
+# make_rocks(1)
+# make_rocks(2)
+# make_rocks(3)
+# make_rocks(4)
 
 
 def move(speed):
@@ -146,6 +73,12 @@ def move(speed):
     elif player_move == "a":
         player.left(90)
         player.forward(20)
+
+    pos = player.position()
+    print(player.position())
+    print(coordinates)
+    if pos in coordinates:
+        print("found")
 
 
 def tager_move(speed):
@@ -189,7 +122,7 @@ tager.left(90)
 tager.pendown
 
 
-print(tager_pos1, tager_pos2)
+# print(tager_pos1, tager_pos2)
 
 
 while i == True:
